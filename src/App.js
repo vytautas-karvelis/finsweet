89807ef65153b31b92a41bcb755256316ba841e5
vytautas-karvelis <vytkarvelis@gmail.com>
2021-10-20 15:36:57 +0200

    finished header_2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Footer1 from './components/Footer1';
+import Footer2 from './components/Footer2';
 import Header from './components/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
           <Header/>
           <Footer1/>
+          <Footer2/>
     </div>
   );
 }
